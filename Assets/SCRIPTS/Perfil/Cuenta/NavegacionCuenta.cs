77452326c4eaf7a58dea385d5cf5f69c7032713d
DuvanR <169104682+Milo2412@users.noreply.github.com>
2025-06-04 17:31:23 -0500

    Primeros Paneles de el modulo cuenta

---
+++ Assets/SCRIPTS/Perfil/Cuenta/NavegacionCuenta.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class NavegacionCuenta : MonoBehaviour
+{
+    // instanciamos los 3 paneles para poder navegar 
+
+    [Header("paneles de navegación")]
+    [SerializeField] public GameObject panelMenuCuenta;
+    [SerializeField] public GameObject panelTerminos_Condiciones;
+    [SerializeField] public GameObject panelPoliticas;
+    [SerializeField] public GameObject panelDatosPersonales;
+    
+    
+
+    public void verMenuCuenta()
+    {
+        panelMenuCuenta.SetActive(true);
+        panelTerminos_Condiciones.SetActive(false);
+        panelPoliticas.SetActive(false);
+        panelDatosPersonales.SetActive(false);
+    }
+    public void verTerminosCondiciones()
+    {
+        panelTerminos_Condiciones.SetActive(true);
+        panelMenuCuenta.SetActive(false);
+        panelPoliticas.SetActive(false);
+        panelDatosPersonales.SetActive(false);
+    }
+   public void verPoliticas()
+    {
+        panelPoliticas.SetActive(true);
+        panelTerminos_Condiciones.SetActive(false);
+        panelMenuCuenta.SetActive(false);
+        panelDatosPersonales.SetActive(false);
+    }
+    public void verDatosPersonales()
+    {
+        panelDatosPersonales.SetActive(true);
+        panelPoliticas.SetActive(false);
+        panelTerminos_Condiciones.SetActive(false);
+        panelMenuCuenta.SetActive(false);
+        
+    }
+
+
+}
